d6888137091291cbc3db40b23281f67296f351d0
pascalmp <pascalmpaw@gmail.com>
2018-06-06 00:17:07 -0400

    event listener

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
+var ourHeadline = document.getElementById("our-headline")
 var listItems = document.getElementById("our-list").getElementsByTagName("li")
 
 for (i = 0; i < listItems.length; i++){
-    listItems[i].innerHTML = "Hello There";
+    listItems[i].addEventListener("click", activateItem);
+}
+function activateItem(){
+    ourHeadline.innerHTML = this.innerHTML;
 }
