bf8231263eecbcd022f1cd01c08cb0f2f8dfdae0
pascalmp <pascalmpaw@gmail.com>
2018-06-10 23:55:11 -0400

    funct

--- misc.js
+++ misc.js
@@ -7,3 +7,6 @@ var listItems = document.getElementById("our-list").getElementsByTagName("li")
 for (i = 0; i < listItems.length; i++){
     listItems[i].addEventListener("click", activateItem);
 }
+function activateItem(){
+    ourHeadline.innerHTML = this.innerHTML;
+}
