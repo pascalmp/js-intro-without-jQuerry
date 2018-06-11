a56bab38fad45ee93ff792ff346891c5c8449269
pascalmp <pascalmpaw@gmail.com>
2018-06-10 23:53:20 -0400

    var

---
+++ misc.js
@@ -0,0 +1,5 @@
+var newItemcounter = 1
+var ourList = document.getElementById("our-list")
+var ourButton = document.getElementById("our-button")
+var ourHeadline = document.getElementById("our-headline")
+var listItems = document.getElementById("our-list").getElementsByTagName("li")
