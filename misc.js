16d7e5221326fbdb20a7dadb3729b728f46c363f
pascalmp <pascalmpaw@gmail.com>
2018-06-10 23:54:27 -0400

    loop

--- misc.js
+++ misc.js
@@ -3,3 +3,7 @@ var ourList = document.getElementById("our-list")
 var ourButton = document.getElementById("our-button")
 var ourHeadline = document.getElementById("our-headline")
 var listItems = document.getElementById("our-list").getElementsByTagName("li")
+
+for (i = 0; i < listItems.length; i++){
+    listItems[i].addEventListener("click", activateItem);
+}
