3a03324e48c0e571ff62fa9cf71c05abfac08a27
pascalmp <pascalmpaw@gmail.com>
2018-06-15 23:46:06 -0400

    remove active

--- js/main.js
+++ js/main.js
@@ -9,6 +9,8 @@ for (i = 0; i < listItems.length; i++){
 }
 function activateItem(){
     ourHeadline.innerHTML = this.innerHTML;
+    for (i = 0; i < listItems.length; i++){
+        listItems[i].classList.remove("active");
     this.classList.add("active");
 }
 
